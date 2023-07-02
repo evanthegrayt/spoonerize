55a401ef1accceb69bf6d0b369704446976e04f9
Evan Gray <evanthegrayt@vivaldi.net>
2023-07-01 23:29:03 -0500

    Config file is now loaded a la rails initializer

--- lib/spoonerize/spoonerism.rb
+++ lib/spoonerize/spoonerism.rb
@@ -9,51 +9,6 @@ module Spoonerize
     attr_reader :words
 
     ##
-    # This boolean determines if flipping should be performed lazily.
-    #
-    # @param [Boolean] true if should be lazy.
-    #
-    # @return [Boolean]
-    attr_writer :lazy
-
-    ##
-    # This boolean determines if flipping should be reversed.
-    #
-    # @param [Boolean] true if should be reversed.
-    #
-    # @return [Boolean]
-    attr_writer :reverse
-
-    ##
-    # The full path to the log file.
-    #
-    # @param [String] file
-    #
-    # @return [String]
-    attr_accessor :logfile_name
-
-    ##
-    # The words that are to be excluded.
-    #
-    # @param [Array] words
-    #
-    # @return [Array]
-    attr_accessor :excluded_words
-
-    ##
-    # The configuration file. Default is +nil+. If set to a string, and the file
-    # exists, it is used to set options.
-    #
-    # @return [String] file path
-    attr_reader :config_file
-
-    ##
-    # The options from +config_file+ as a hash.
-    #
-    # @return [Hash] Options from +config_file+
-    attr_reader :config
-
-    ##
     # Initialize instance. You can also use the +config_file+ either by passing
     # it at initialization, or via the setter. The config file will be
     # automatically loaded if passed at initialization, before the instance is
@@ -76,18 +31,11 @@ module Spoonerize
     #  end
     #  s.load_config_file # Would override setting from initialization
     def initialize(words, config_file = nil)
-      @config = {}
-      @excluded_words = []
       @words = words.map(&:downcase)
-      @lazy = false
-      @reverse = false
       @config_file = config_file && File.expand_path(config_file)
       @config_file_loaded = false
-      @logfile_name = File.expand_path(
-        File.join(ENV["HOME"], ".cache", "spoonerize", "spoonerize.csv")
-      )
 
-      load_config_file if config_file
+      Spoonerize.load_config_file if config_file
 
       yield self if block_given?
     end
@@ -120,29 +68,9 @@ module Spoonerize
     end
 
     ##
-    # Has a config file been loaded?
-    #
-    # @return [Boolean]
-    def config_file_loaded?
-      @config_file_loaded
-    end
-
-    ##
     # Returns true if there are more than one non-excluded word to flip
     def enough_flippable_words?
       (words - all_excluded_words).size > 1
-    end
-
-    ##
-    # Should the lazy words be excluded?
-    def lazy?
-      @lazy
-    end
-
-    ##
-    # Should the words flip the other direction?
-    def reverse?
-      @reverse
     end
 
     ##
@@ -157,30 +85,7 @@ module Spoonerize
     # * Any user-passed words, stored in +excluded_words+
     # * If lazy-mode, the LAZY_WORDS from yaml file are added
     def all_excluded_words
-      (excluded_words + (lazy? ? LAZY_WORDS : [])).map(&:downcase)
-    end
-
-    ##
-    # Setter for +config_file+. Must be expanded in case the user uses `~` for
-    # home.
-    #
-    # @param [String] file
-    #
-    # @return [String]
-    def config_file=(config_file)
-      @config_file = File.expand_path(config_file)
-    end
-
-    ##
-    # Loads the config file
-    #
-    # @return [Hash] The config options
-    def load_config_file
-      raise "No config file set" if config_file.nil?
-      raise "File #{config_file} does not exist" unless File.file?(config_file)
-      @config = YAML.load_file(config_file)
-      @config_file_loaded = true
-      @config.each { |k, v| send("#{k}=", v) }
+      (Spoonerize.config.excluded_words + (Spoonerize.config.lazy? ? Spoonerize.config.lazy_words : [])).map(&:downcase)
     end
 
     private
@@ -224,9 +129,9 @@ module Spoonerize
     # the options as a string
     def options # :nodoc:
       [].tap do |o|
-        o << "Lazy" if lazy?
-        o << "Reverse" if reverse?
-        o << "Exclude [#{all_excluded_words.join(", ")}]" if excluded_words.any?
+        o << "Lazy" if Spoonerize.config.lazy?
+        o << "Reverse" if Spoonerize.config.reverse?
+        o << "Exclude [#{all_excluded_words.join(", ")}]" if Spoonerize.config.excluded_words.any?
         o << "No Options" if o.empty?
       end
     end
