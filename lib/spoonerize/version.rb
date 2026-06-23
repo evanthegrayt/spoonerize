fb12dfe520ed4c62def6deb7337ca258933b384d
Evan Gray <evanthegrayt@vivaldi.net>
2026-06-23 18:25:14 -0500

    Version 1.0

--- lib/spoonerize/version.rb
+++ lib/spoonerize/version.rb
@@ -9,13 +9,13 @@ module Spoonerize
     # Major version.
     #
     # @return [Integer]
-    MAJOR = 0
+    MAJOR = 1
 
     ##
     # Minor version.
     #
     # @return [Integer]
-    MINOR = 3
+    MINOR = 0
 
     ##
     # Patch version.
